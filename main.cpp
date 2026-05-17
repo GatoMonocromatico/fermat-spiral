d7fcd1f4d19b8287a1a58ac4f186d69cee8b5c3e
ivo <ivoarpino2@gmail.com>
2026-05-17 17:27:21 -0300

    adapting output for geogebra .xml file

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include<numbers>
 #include<iostream>
 
+// #define DEBUG_OUTPUT
+
 constexpr float kEpsilon = 1e-5f;
 
 struct point {
@@ -32,11 +34,13 @@ bool solveFermatSpiral(std::vector<point>& vertices, double constant, double dis
     while (angle < limitArc)
     {
         point lastPoint = vertices.back();
+#ifdef DEBUG_OUTPUT
         std::cout << "[outer #" << iterator1 << "]"
                   << " angle=" << angle
                   << " remaining=" << (limitArc - angle)
                   << " lastPoint=(r=" << lastPoint.module << ", a=" << lastPoint.angle << ")"
                   << "\n";
+#endif
         iterator1 += 1;
 
         size_t iterator2 = 0;
@@ -45,7 +49,7 @@ bool solveFermatSpiral(std::vector<point>& vertices, double constant, double dis
 
         double ray = constant * sqrt(angle);
 
-        double targetDistance = distanceBetweenPoints + (lastPoint.angle/limitArc) * distanceBetweenPoints * 16;
+        double targetDistance = distanceBetweenPoints + (lastPoint.angle/limitArc) * distanceBetweenPoints * 10;
 
         double actualDistanceToLastPoint = sqrt(pow(lastPoint.module, 2) + pow(ray, 2) - 2 * ray * lastPoint.module * cos(angle - lastPoint.angle));
 
@@ -53,6 +57,7 @@ bool solveFermatSpiral(std::vector<point>& vertices, double constant, double dis
         while (std::abs(targetDistance - actualDistanceToLastPoint) > kEpsilon)
         {
             double error = targetDistance - actualDistanceToLastPoint;
+#ifdef DEBUG_OUTPUT
             std::cout << "  [inner #" << iterator2 << "]"
                       << " angle=" << angle
                       << " ray=" << ray
@@ -61,20 +66,25 @@ bool solveFermatSpiral(std::vector<point>& vertices, double constant, double dis
                       << " error=" << error
                       << " stepAngle=" << stepAngle
                       << "\n";
+#endif
             iterator2 += 1;
 
             if (actualDistanceToLastPoint < targetDistance)
             {
                 angle += stepAngle;
                 ray = constant * sqrt(angle);
+#ifdef DEBUG_OUTPUT
                 std::cout << "    -> dist < target, increased angle to " << angle << "\n";
+#endif
             }
             else if (actualDistanceToLastPoint > targetDistance)
             {
                 stepAngle /= adjustmentFactor;
                 angle -= stepAngle;
                 ray = constant * sqrt(angle);
+#ifdef DEBUG_OUTPUT
                 std::cout << "    -> dist > target, halved stepAngle to " << stepAngle << ", decreased angle to " << angle << "\n";
+#endif
             }
             else {std::cout << "nao sabo porque saiu aqui, mas saiu..."; break;}
             actualDistanceToLastPoint = sqrt(pow(lastPoint.module, 2) + pow(ray, 2) - 2 * ray * lastPoint.module * cos(angle - lastPoint.angle));
@@ -86,7 +96,9 @@ bool solveFermatSpiral(std::vector<point>& vertices, double constant, double dis
         //         j-=1;
         //     }
         }
+#ifdef DEBUG_OUTPUT
         std::cout << "  [inner done] converged: angle=" << angle << " ray=" << ray << " dist=" << actualDistanceToLastPoint << "\n";
+#endif
         vertices.push_back(point(ray, angle));
     }
     
@@ -112,7 +124,21 @@ int main()
 
         double constant = p1.y() - (p1.x() * coeficient);
 
-        std::cout << "Curva(-" << inverseCoeficient << "*t, "  << constant << "- t"", t, " << constant - std::max(p1.y(), p2.y()) << ", " << constant - std::min(p1.y(), p2.y()) << ")\n";
+        std::cout << "<command name=\"CurveCartesian\">\n"
+                  << "    <input a0=\"" << inverseCoeficient << "*t\""
+                  << " a1=\"" << constant << "- t\""
+                  << " a2=\"t\""
+                  << " a3=\"" << (constant - std::max(p1.y(), p2.y())) << "\""
+                  << " a4=\"" << (constant - std::min(p1.y(), p2.y())) << "\"/>\n"
+                  << "    <output a0=\"k\"/>\n"
+                  << "</command>\n"
+                  << "<element type=\"curvecartesian\" label=\"k\">\n"
+                  << "    <show object=\"true\" label=\"false\"/>\n"
+                  << "    <objColor r=\"0\" g=\"103\" b=\"88\" alpha=\"0\"/>\n"
+                  << "    <layer val=\"0\"/>\n"
+                  << "    <labelMode val=\"0\"/>\n"
+                  << "    <lineStyle thickness=\"5\" type=\"0\" typeHidden=\"1\" opacity=\"204\"/>\n"
+                  << "</element>\n";
     }
     for (size_t i = 0; i + 1 < vertices.size(); i++)
     {
@@ -128,10 +154,24 @@ int main()
 
         double constant = p1.y() - (p1.x() * coeficient);
 
-        std::cout << "Curva(-" << inverseCoeficient << "*t, "  << constant << "- t"", t, " << constant - std::max(p1.y(), p2.y()) << ", " << constant - std::min(p1.y(), p2.y()) << ")\n";
+        std::cout << "<command name=\"CurveCartesian\">\n"
+                  << "    <input a0=\"" << inverseCoeficient << "*t\""
+                  << " a1=\"" << constant << "- t\""
+                  << " a2=\"t\""
+                  << " a3=\"" << (constant - std::max(p1.y(), p2.y())) << "\""
+                  << " a4=\"" << (constant - std::min(p1.y(), p2.y())) << "\"/>\n"
+                  << "    <output a0=\"k\"/>\n"
+                  << "</command>\n"
+                  << "<element type=\"curvecartesian\" label=\"k\">\n"
+                  << "    <show object=\"true\" label=\"false\"/>\n"
+                  << "    <objColor r=\"0\" g=\"103\" b=\"88\" alpha=\"0\"/>\n"
+                  << "    <layer val=\"0\"/>\n"
+                  << "    <labelMode val=\"0\"/>\n"
+                  << "    <lineStyle thickness=\"5\" type=\"0\" typeHidden=\"1\" opacity=\"204\"/>\n"
+                  << "</element>\n";
     }
 
-    std::cout << "amount of segments=" << vertices.size() - 1;
+    std::cout << "amount of segments=" << (vertices.size() - 1) * 2;
 
     return 1;
 }
